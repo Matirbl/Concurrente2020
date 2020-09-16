dfcbb65c58ccbbb7cd1791ec46aae83defd193c9
Chino <Chino@DESKTOP-D5UB2E4>
2020-09-16 17:52:13 -0300

    Se realizó el punto 4 del tp 3

---
+++ src/Tp3Punto4/newpackage/Main.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Tp3Punto4.newpackage;
+
+/**
+ *
+ * @author Chino
+ */
+public class Main {
+
+    public static void main(String[] args) {
+
+        Surtidor surti = new Surtidor(1);
+        String[] abc = {"abc", "def", "ghi", "jkl", "mnñ"};
+
+        Thread[] autos = new Thread[5];
+
+        for (int i = 0; i < autos.length; i++) {
+            autos[i] = new Thread(new Auto(surti, abc[i]));
+
+        }
+
+        for (int i = 0; i < autos.length; i++) {
+            autos[i].start();
+
+        }
+
+    }
+
+}
