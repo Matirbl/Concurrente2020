643ca0d702c959f1e5b0e1a4fd8bcb9c9bfecab6
Chino <Chino@DESKTOP-D5UB2E4>
2020-09-16 17:55:34 -0300

    Se realizaron modificaciones en la clase Auto

--- src/Tp3/Punto2Corregido/Curandero.java
+++ src/Tp3/Punto2Corregido/Curandero.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package Tp3.Punto2Corregido.newpackage;
+package Tp3.Punto2Corregido;
 
 import Tp3.Punto2.*;
 
